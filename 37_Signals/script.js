b0a4b4bc9b873ae393a909ae7c103d0377f57ab8
LOrdaz <kingluwey@gmail.com>
2014-02-21 00:22:35 -0700

    37Signals

---
+++ 37_Signals/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+	$('#one').mouseenter(function(){
+	$('#top-verbaige').css('font-size',"48px");
+	$('#top-verbaige').html('BaseCamp is the project management tool you<br>wish you had on your last project.');
+	$('#bottom-verbaige').css('font-size',"18x");
+	$('#bottom-verbaige').html("Are you still managing projects with email? Are you still using Excel for your to-do lists? It's time to upgrade to<br>Basecamp. Manage projects and collaborate with your team and clients the modern way.");
+	$('#arrow1').css('display','inline')
+	}).mouseleave(function() {
+		$('#arrow1').css('display','hidden')
+		$('#top-verbaige').css('font-size',"58px");
+		$('#bottom-verbaige').css('font-size',"20x");
+		$('#top-verbaige').html('Making collaboration productive and<br>enjoyable for people every day.</p>');
+		$('#bottom-verbaige').html("Frustration-free web-based apps for collaboration, sharing information, and making decisions.");
+	});
+		$('#two').mouseenter(function(){
+	$('#top-verbaige').css('font-size',"48px");
+	$('#top-verbaige').html('Highrise remembers the important things<br>about people you\'d normally forget.');
+	$('#bottom-verbaige').css('font-size',"18x");
+	$('#bottom-verbaige').html("Keep a permanent record of people you do buisness with. know who you talked to,when you talked to them,<br>what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.");
+	$('#arrow2').css('display','inline')
+	}).mouseleave(function() {
+		$('#arrow2').css('display','hidden')
+		$('#top-verbaige').css('font-size',"58px");
+		$('#bottom-verbaige').css('font-size',"20x");
+		$('#top-verbaige').html('Making collaboration productive and<br>enjoyable for people every day.</p>');
+		$('#bottom-verbaige').html("Frustration-free web-based apps for collaboration, sharing information, and making decisions.");
+	});
+		$('#three').mouseenter(function(){
+	$('#top-verbaige').css('font-size',"48px");
+	$('#top-verbaige').html('From near or far, Campfire helps teams work<br>together over the web in real-time.');
+	$('#bottom-verbaige').css('font-size',"18x");
+	$('#bottom-verbaige').html("Share ideas,discussions, concepts,images,code samples, videos, mockups, and documents in a real-time<br>private chat room. It\'s game changing. We couldn\'t run our own buisness without Campfire.");
+	$('#arrow3').css('display','inline')
+	}).mouseleave(function() {
+		$('#arrow3').css('display','hidden')
+		$('#top-verbaige').css('font-size',"58px");
+		$('#bottom-verbaige').css('font-size',"20x");
+		$('#top-verbaige').html('Making collaboration productive and<br>enjoyable for people every day.</p>');
+		$('#bottom-verbaige').html("Frustration-free web-based apps for collaboration, sharing information, and making decisions.");
+	});
+});
